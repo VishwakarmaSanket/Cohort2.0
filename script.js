aa58ec8bc815e397fdb137cd7d00e8562eead7bb
Sanket Vishwakarma <your-email@example.com>
2025-11-13 23:19:26 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+// COHORT 2.0 JS Questions part - 3
+
+// 1. Print all numbers from 1 to 10 using loop
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// 2. Print only even numbers in range from 1 to 20 using loop
+// for (let i = 1; i <= 20; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// 3.Print numbers from 10 to 1 in reverse order using decrement
+// for (let i = 10; i >= 1; i--) {
+//   console.log(i);
+// }
+
+// 4. Print the word "Yes" 5 times repeat using loop
+// for (let i = 1; i <= 5; i++) {
+//   console.log("Yes");
+// }
+
+// 5. Print whether numbers from 1 to 10 are even or odd
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 === 0) {
+//     console.log(`${i} : is even`);
+//   } else {
+//     console.log(`${i} : is odd`);
+//   }
+// }
+
+// 6. Ask for a number and say if it is neagtive or positive using conditionals and prompt
+// let num = +prompt("Enter the number : ");
+// if (num >= 0) {
+//   console.log(`${num} is positive`);s
+// } else {
+//   console.log(`${num} is neagtive`);
+// }
+
+// 7.
+// let age = prompt("Enter your age : ");
+// if (age === null) {
+//   console.error("You Pressed Cancel.");
+// } else if (age.trim === "") {
+//   console.error("Enter a valid age number");
+// } else if (parseInt(age) >= 18) {
+//   console.log("You can vote");
+// } else {
+//   console.log("You can't vote");
+// }
